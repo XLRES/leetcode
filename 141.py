17825e1b47f56ad4af4d6ab78bfe799d701c5ed8
XLRES <xlresistor@gmail.com>
2015-05-08 21:29:16 +0800

    add #141 Linked List Cycle and update README.md

--- 141.py
+++ 141.py
@@ -1,3 +1,12 @@
 class Solution:
     def hasCycle(self,head):
+        p1 = p2 = head
+        p1 = p1.next
+        p2 = p2.next.next
+        while(p1 != none and p2 != none):
+            p1 = p1.next
+            p2 = p2.next.next
+            if (p1 == p2 ): break
+        if p1==p2 : return True
+        else return False
 
